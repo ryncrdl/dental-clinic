4dc5d8ac50325a067d2cba98a2f2379eeec2b9c1
Ryan Cordial <ryancordial.s@gmail.com>
2023-07-10 12:32:49 +0800

    update all files

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,27 @@
-import React from "react"
+import React from "react";
 
 const Contact = () => {
   return (
     <section id="contact">
       <h1>Contact Us</h1>
-    </section>
-  )
-}
 
-export default Contact
+      <form id="contact-container">
+        <div className="field-container">
+          <label htmlFor="name">Name</label>
+          <input type="text" placeholder="e.g John Doe" id="name" />
+        </div>
+        <div className="field-container">
+          <label htmlFor="subject">Subject</label>
+          <input type="text" placeholder="e.g Request Refund" id="subject" />
+        </div>
+        <div className="field-container">
+          <label htmlFor="message">Message</label>
+          <textarea type="text" id="message" />
+        </div>
+        <button>Submit</button>
+      </form>
+    </section>
+  );
+};
+
+export default Contact;
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,9 @@ const Footer = () => {
   return (
     <footer>
       <h1>Footer</h1>
+      <div id="footer-container"></div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
