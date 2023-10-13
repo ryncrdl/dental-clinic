3937067ae077151b3136eac824f665ee17e8cb96
Ryan Cordial <ryancordial.s@gmail.com>
2023-10-14 02:55:29 +0800

    updates responsiveness

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,10 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import logo from "../assets/logo.png"
 
 const Header = () => {
   const [scroll, setScroll] = useState(false)
   const [close, setClose] = useState(false)
+  const [innerWidth, setInnerWidth] = useState(window.innerWidth)
 
   const scrollToHome = () => {
     window.scrollTo(0, 0)
@@ -18,6 +19,17 @@ const Header = () => {
     }
   }
 
+  useEffect(() => {
+    const updateInnerWidth = () => {
+      setInnerWidth(window.innerWidth)
+      setClose(false)
+    }
+    window.addEventListener("resize", updateInnerWidth)
+    return () => {
+      window.removeEventListener("resize", updateInnerWidth)
+    }
+  }, [])
+
   const onOpenNavigation = () => {
     setClose(true)
   }
@@ -26,45 +38,56 @@ const Header = () => {
     setClose(false)
   }
 
-  return (
-    <header
-      style={
-        scroll
-          ? { padding: "clamp(0.5rem, 4vw, 1rem) clamp(2rem, 4vw, 4rem)" }
-          : null
-      }
-    >
-      <a href="#home" id="logo" onClick={onOpenNavigation}>
-        <img src={logo} alt="Logo" />
+  const responsiveNavagation = (
+    <>
+      <a href="#home" onClick={scrollToHome}>
+        Home
       </a>
-      <button id="btn-nav" onClick={onOpenNavigation}>
-        &#9776;
-      </button>
-      <nav id="nav-desktop" className={close ? "open" : null}>
-        <button
-          id="btn-nav-close"
-          className={close ? "open" : "close"}
-          onClick={() => onCloseNavigation()}
-        >
-          &#x2715;
+      <a href="#services-container" onClick={onCloseNavigation}>
+        Services
+      </a>
+      <a href="#blogs-container" onClick={onCloseNavigation}>
+        Blogs
+      </a>
+      <a href="#about" onClick={onCloseNavigation}>
+        About
+      </a>
+      <a href="#contact" onClick={onCloseNavigation}>
+        Contact Us
+      </a>
+    </>
+  )
+
+  return (
+    <>
+      {innerWidth <= 800 && (
+        <nav id="res-nav" className={close ? "open" : null}>
+          <button
+            id="btn-nav-close"
+            className={close ? "open" : "close"}
+            onClick={() => onCloseNavigation()}
+          >
+            &#x2715;
+          </button>
+          {responsiveNavagation}
+        </nav>
+      )}
+      <header
+        style={
+          scroll
+            ? { padding: "clamp(0.5rem, 4vw, 1rem) clamp(2rem, 4vw, 4rem)" }
+            : null
+        }
+      >
+        <a href="#home" id="logo" onClick={onOpenNavigation}>
+          <img src={logo} alt="Logo" />
+        </a>
+        <button id="btn-nav" onClick={onOpenNavigation}>
+          &#9776;
         </button>
-        <a href="#home" onClick={scrollToHome}>
-          Home
-        </a>
-        <a href="#services-container" onClick={() => onCloseNavigation()}>
-          Services
-        </a>
-        <a href="#blogs-container" onClick={() => onCloseNavigation()}>
-          Blog
-        </a>
-        <a href="#about" onClick={() => onCloseNavigation()}>
-          About
-        </a>
-        <a href="#contact" onClick={() => onCloseNavigation()}>
-          Contact Us
-        </a>
-      </nav>
-    </header>
+        {innerWidth > 800 && <nav id="nav-desktop">{responsiveNavagation}</nav>}
+      </header>
+    </>
   )
 }
 
